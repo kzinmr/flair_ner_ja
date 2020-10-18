427fc446295a63d949aea7551dd349185c73b880
Kazuki Inamura <kzinmr109@gmail.com>
2020-10-18 15:08:17 +0900

    fix: add training on gsd corpus

--- app.py
+++ app.py
@@ -25,7 +25,9 @@ class ConllConverter:
         return self.wakati.parse(text).split()
 
     @staticmethod
-    def get_superspan(query_span: Tuple[int, int], superspans: List[Tuple[int, int]]) -> Optional[Tuple[int, int]]:
+    def get_superspan(
+        query_span: Tuple[int, int], superspans: List[Tuple[int, int]]
+    ) -> Optional[Tuple[int, int]]:
         """ return superspan for given query span from set of superspans if any
         """
         for superspan in superspans:
@@ -34,7 +36,12 @@ class ConllConverter:
         return None
 
     @classmethod
-    def get_token_labels(cls, token_spans: Tuple[int, int], chunk_spans: Tuple[int, int], chunk_labels: List[str]) -> List[str]:
+    def get_token_labels(
+        cls,
+        token_spans: Tuple[int, int],
+        chunk_spans: Tuple[int, int],
+        chunk_labels: List[str],
+    ) -> List[str]:
         """ chunk単位のラベルから、token単位のラベルを構成
         """
 
@@ -51,23 +58,24 @@ class ConllConverter:
         tokenspan2tagtype = dict(zip(target_token_spans, tagtypes))
 
         # token に対応する label をchunkのtagtypeを基に構成
-        label = 'O'
+        label = "O"
         token_labels = []
         for token_span in token_spans:
             if token_span in tokenspan2tagtype:
                 tagtype = tokenspan2tagtype[token_span]
-                if label == 'O':
-                    label = f'B-{tagtype}'
+                if label == "O":
+                    label = f"B-{tagtype}"
                 else:
-                    label = f'I-{tagtype}'
+                    label = f"I-{tagtype}"
             else:
-                label = 'O'
+                label = "O"
 
             token_labels.append(label)
         return token_labels
 
-
-    def tokenize_and_align_spans(self, text: str, chunk_spans: List[Tuple[int, int]], chunk_labels: List[str]) -> List[Tuple[str, str]]:
+    def tokenize_and_align_spans(
+        self, text: str, chunk_spans: List[Tuple[int, int]], chunk_labels: List[str]
+    ) -> List[Tuple[str, str]]:
 
         # text -> tokens
         tokens = self.tokenize(text)
@@ -79,71 +87,108 @@ class ConllConverter:
         token_labels = self.get_token_labels(token_spans, chunk_spans, chunk_labels)
 
         # CoNLL2003-likeなtoken行単位の列データを返す
-        token_label_columns = [(token, label) for token, label in zip(tokens, token_labels)]
+        token_label_columns = [
+            (token, label) for token, label in zip(tokens, token_labels)
+        ]
         # 計算したspanが合ってるかのassertion
-        spannedtoken_label_columns = [(text[span[0]:span[1]], label) for span, label in zip(token_spans, token_labels)]
+        spannedtoken_label_columns = [
+            (text[span[0] : span[1]], label)
+            for span, label in zip(token_spans, token_labels)
+        ]
         assert spannedtoken_label_columns == token_label_columns
 
         return token_label_columns
 
-def download_conll_data(filepath):
-    """ conllフォーマットデータのダウンロード
-    """
-    url = 'https://raw.githubusercontent.com/Hironsan/IOB2Corpus/master/ja.wikipedia.conll'
+
+def download_data(url, filepath):
     response = requests.get(url)
     if response.ok:
         with open(filepath, "w") as fp:
-            fp.write(response.content.decode('utf8'))
+            fp.write(response.content.decode("utf8"))
         return filepath
+
 
 def make_sample_conll_corpus(data_folder):
     """ conllフォーマットデータのダウンロード
     """
-    train_file = 'ja.wikipedia.conll'
+    url = "https://raw.githubusercontent.com/Hironsan/IOB2Corpus/master/ja.wikipedia.conll"
+    train_file = "ja.wikipedia.conll"
     filepath = data_folder / train_file
-    if download_conll_data(filepath):
-        columns = {0: 'text', 1: 'ner'}
-        corpus = ColumnCorpus(data_folder, columns,
-                              train_file=train_file)
+    if download_data(url, filepath):
+        columns = {0: "text", 1: "ner"}
+        corpus = ColumnCorpus(data_folder, columns, train_file=train_file)
 
         return corpus
+
+
+def make_gsd_conll_corpus(data_folder):
+    """ conllフォーマットデータのダウンロード
+    """
+    train_url = "https://github.com/megagonlabs/UD_Japanese-GSD/releases/download/v2.6-NE/ja_gsd-ud-train.ne.conllu"
+    train_file = "ja_gsd-ud-train.ne.conllu"
+    train_path = data_folder / train_file
+    dev_url = "https://github.com/megagonlabs/UD_Japanese-GSD/releases/download/v2.6-NE/ja_gsd-ud-dev.ne.conllu"
+    dev_file = "ja_gsd-ud-dev.ne.conllu"
+    dev_path = data_folder / dev_file
+    test_url = "https://github.com/megagonlabs/UD_Japanese-GSD/releases/download/v2.6-NE/ja_gsd-ud-test.ne.conllu"
+    test_file = "ja_gsd-ud-test.ne.conllu"
+    test_path = data_folder / test_file
+    if (
+        download_data(train_url, train_path)
+        and download_data(dev_url, dev_path)
+        and download_data(test_url, test_path)
+    ):
+        columns = {0: "text", 1: "ner"}
+        corpus = ColumnCorpus(
+            data_folder,
+            columns,
+            train_file=train_file,
+            dev_file=dev_file,
+            test_file=test_file,
+        )
+        return corpus
+
 
 def make_conll(cc, reader):
     conll = []
     for text, entd in reader.iter():
-        labels = [label for _, _, label in entd['entities']]
-        spans = [(s, e) for s, e, _ in entd['entities']]
+        labels = [label for _, _, label in entd["entities"]]
+        spans = [(s, e) for s, e, _ in entd["entities"]]
         tokens_labels = cc.tokenize_and_align_spans(text, spans, labels)
-        conll.append("\n".join([f"{token}\t{label}" for (token, label) in tokens_labels]))
+        conll.append(
+            "\n".join([f"{token}\t{label}" for (token, label) in tokens_labels])
+        )
     return conll
+
 
 def make_conll_corpus(data_folder):
     """ camphr形式からtokenize & conllフォーマットに変換し、Corpus化
     """
     cc = ConllConverter()
-    train_file = 'train.jsonl'
-    test_file = 'test.jsonl'
-    with jsonlines.open(data_folder / train_file, 'r') as reader:
+    train_file = "train.jsonl"
+    test_file = "test.jsonl"
+    with jsonlines.open(data_folder / train_file, "r") as reader:
         conll = make_conll(cc, reader)
-    train_file = 'train.conll'
-    with open(data_folder / train_file, 'w') as fp:
+    train_file = "train.conll"
+    with open(data_folder / train_file, "w") as fp:
         fp.write("\n\n".join(conll))
 
-    with jsonlines.open(data_folder / test_file, 'r') as reader:
+    with jsonlines.open(data_folder / test_file, "r") as reader:
         conll = make_conll(cc, reader)
-    test_file = 'test.conll'
-    with open(data_folder / test_file, 'w') as fp:
+    test_file = "test.conll"
+    with open(data_folder / test_file, "w") as fp:
         fp.write("\n\n".join(conll))
 
-    columns = {0: 'text', 1: 'ner'}
-    corpus = ColumnCorpus(data_folder, columns,
-                          train_file=train_file,
-                          test_file=test_file)
+    columns = {0: "text", 1: "ner"}
+    corpus = ColumnCorpus(
+        data_folder, columns, train_file=train_file, test_file=test_file
+    )
 
     return corpus
 
+
 def make_tagger(corpus, cfg):
-    tag_type = 'ner'
+    tag_type = "ner"
     tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
     print(tag_dictionary.idx2item)
 
@@ -151,43 +196,50 @@ def make_tagger(corpus, cfg):
     # using a character-level language model
     # Trained with 439M words of Japanese Web crawls
     embedding_types = [
-        FlairEmbeddings('ja-forward'),
-        FlairEmbeddings('ja-backward'),
+        FlairEmbeddings("ja-forward"),
+        FlairEmbeddings("ja-backward"),
     ]
     embeddings = StackedEmbeddings(embeddings=embedding_types)
 
-    tagger = SequenceTagger(hidden_size=cfg.model.hidden_size,
-                            dropout=cfg.model.dropout,
-                            word_dropout=cfg.model.word_dropout,
-                            locked_dropout=cfg.model.locked_dropout,
-                            embeddings=embeddings,
-                            tag_dictionary=tag_dictionary,
-                            tag_type=tag_type,
-                            use_crf=True)
+    tagger = SequenceTagger(
+        hidden_size=cfg.model.hidden_size,
+        dropout=cfg.model.dropout,
+        word_dropout=cfg.model.word_dropout,
+        locked_dropout=cfg.model.locked_dropout,
+        embeddings=embeddings,
+        tag_dictionary=tag_dictionary,
+        tag_type=tag_type,
+        use_crf=True,
+    )
     return tagger
+
 
 @hydra.main(config_name="config")
 def main(cfg: DictConfig):
-    print(f'Current working directory: {os.getcwd()}')
-    print(f'Orig working directory : {hydra.utils.get_original_cwd()}')
-    print(f'to_absolute_path("outputs/data") : {hydra.utils.to_absolute_path("outputs/data")}')
+    print(f"Current working directory: {os.getcwd()}")
+    print(f"Orig working directory : {hydra.utils.get_original_cwd()}")
+    print(
+        f'to_absolute_path("outputs/data") : {hydra.utils.to_absolute_path("outputs/data")}'
+    )
 
-    data_folder = Path(hydra.utils.get_original_cwd()) / 'outputs/data'
-    corpus = make_conll_corpus(data_folder)
+    data_folder = Path(hydra.utils.get_original_cwd()) / "outputs/data"
+    corpus = make_gsd_conll_corpus(data_folder)
     tagger = make_tagger(corpus, cfg)
     trainer = ModelTrainer(tagger, corpus)
-    model_dir = Path(hydra.utils.get_original_cwd()) / 'outputs/models'
-    trainer.train(model_dir,
-                  learning_rate=cfg.training.learning_rate,
-                  mini_batch_size=cfg.training.mini_batch_size,
-                  max_epochs=cfg.training.max_epochs,
-                  patience=cfg.training.patience,
-                  initial_extra_patience=cfg.training.initial_extra_patience,
-                  anneal_factor=cfg.training.anneal_factor,
-                  shuffle=cfg.training.shuffle,
-                  train_with_dev=cfg.training.train_with_dev,
-                  batch_growth_annealing=cfg.training.batch_growth_annealing,
-                  )
+    model_dir = Path(hydra.utils.get_original_cwd()) / "outputs/models"
+    trainer.train(
+        model_dir,
+        learning_rate=cfg.training.learning_rate,
+        mini_batch_size=cfg.training.mini_batch_size,
+        max_epochs=cfg.training.max_epochs,
+        patience=cfg.training.patience,
+        initial_extra_patience=cfg.training.initial_extra_patience,
+        anneal_factor=cfg.training.anneal_factor,
+        shuffle=cfg.training.shuffle,
+        train_with_dev=cfg.training.train_with_dev,
+        batch_growth_annealing=cfg.training.batch_growth_annealing,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
